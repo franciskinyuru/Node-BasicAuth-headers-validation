2e6c08066b32db005ab58fb9b386d1c70f091b5d
francis kinyuru <franciskinyuru26@gmail.com>
2023-02-13 17:55:19 +0300

    node with express Basic-Auth and Headers validation

---
+++ READme.md
@@ -0,0 +1,44 @@
+# Node-Express BasicAuth and Headers validation
+
+# Required packages
+-  express
+-  express-basic-auth
+-  dotenv
+-  nodemon
+
+# Installation
+
+Install dependencies by running 
+
+ ` npm install `
+
+ Creat a .env file and add below variables and provide your details
+
+MY_SECRET_USERNAME
+
+MY_SECRET_PASSWORD
+
+SOURCE_SYSTEM
+
+ROUTER_ID
+ 
+
+# Running the Application 
+
+Run below command the test you application on postman
+
+` npm start`
+
+
+# Results
+
+Request BasicAuth section and response
+<img width="1114" alt="Screenshot 2023-02-13 at 16 55 53" src="https://user-images.githubusercontent.com/49100234/218486319-43da0954-3454-4275-9984-4ed21687b584.png">
+
+Request headers and response
+<img width="1114" alt="Screenshot 2023-02-13 at 16 55 58" src="https://user-images.githubusercontent.com/49100234/218486305-3a346663-9ec5-4db6-8ba8-6085788a369d.png">
+
+
+
+
+
---
+++ main.js
@@ -0,0 +1,40 @@
+const express = require('express')
+const app = express()
+const basicAuth = require('express-basic-auth')
+require('dotenv').config()
+const router = express.Router()
+
+app.use(basicAuth({
+    authorizer: (username, password,cb) =>{
+        const userMatches = basicAuth.safeCompare(username, process.env.MY_SECRET_USERNAME)
+        const passwordMatches = basicAuth.safeCompare(password,process.env.MY_SECRET_PASSWORD)
+        if (userMatches & passwordMatches) {
+            return cb(null, true)
+        } else {
+           return cb(null, false) 
+        } 
+    },
+    authorizeAsync:true
+}))
+
+app.use((req, res, next)=>{
+    const router_id= req.headers['x-router-id']
+    const source_system = req.headers['x-source-system']
+    if (source_system === process.env.SOURCE_SYSTEM && router_id===process.env.ROUTER_ID) {
+        next()
+    } else {
+        res.send(401).json({msg:"unauthorized requests"})
+    }
+})
+
+
+
+app.get('/', (req, res) =>{
+    res.status(200).json({status:200,msg:"request sent successfully"})
+})
+
+const PORT = 4040
+
+app.listen(PORT, ()=>{
+console.log(`server listening on ${PORT} ...`);
+})
